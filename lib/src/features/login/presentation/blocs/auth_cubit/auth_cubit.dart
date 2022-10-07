c9763945843f3698537ba357362f981d32ff8798
Marcelo Moresco <marcelomoresco0@gmail.com>
2022-10-07 13:07:55 -0300

    feature user cubit and auth cubit

--- lib/src/features/login/presentation/blocs/auth_cubit/auth_cubit.dart
+++ lib/src/features/login/presentation/blocs/auth_cubit/auth_cubit.dart
@@ -1,8 +1,55 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+
+import '../../../domain/usecases/get_current_uid_usecase.dart';
+import '../../../domain/usecases/is_sign_in_usecase.dart';
+import '../../../domain/usecases/sign_out_usecase.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitial());
+  final GetCurrentUIdUsecase getCurrentUIdUsecase;
+  final IsSignInUsecase isSignInUsecase;
+  final SignOutUsecase signOutUsecase;
+
+  AuthCubit({
+    required this.getCurrentUIdUsecase,
+    required this.isSignInUsecase,
+    required this.signOutUsecase,
+  }) : super(AuthInitial());
+
+  Future<void> appStarted() async {
+    try {
+      final isSignIn = await isSignInUsecase.call();
+
+      if (isSignIn) {
+        final uid = await getCurrentUIdUsecase.call();
+        emit(Authenticated(uid: uid));
+      } else {
+        emit(UnAuthenticated());
+      }
+    } on SocketException catch (_) {
+      emit(UnAuthenticated());
+    }
+  }
+
+  Future<void> loggedIn() async {
+    try {
+      final uid = await getCurrentUIdUsecase.call();
+      emit(Authenticated(uid: uid));
+    } on SocketException catch (_) {
+      emit(UnAuthenticated());
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await signOutUsecase.call();
+      emit(UnAuthenticated());
+    } on SocketException catch (_) {
+      emit(UnAuthenticated());
+    }
+  }
 }
--- lib/src/features/login/presentation/blocs/user_cubit/user_cubit.dart
+++ lib/src/features/login/presentation/blocs/user_cubit/user_cubit.dart
@@ -1,8 +1,45 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../domain/entities/user_entity.dart';
+import '../../../domain/usecases/get_create_current_user_usecase.dart';
+import '../../../domain/usecases/sign_in_usecase.dart';
+import '../../../domain/usecases/sign_out_usecase.dart';
+import '../../../domain/usecases/sign_up_usecase.dart';
+
 part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> {
-  UserCubit() : super(UserInitial());
+  final SignInUsecase signInUsecase;
+  final SignUpUsecase signUpUsecase;
+  final SignOutUsecase signOutUsecase;
+  final GetCreateCurrentUserUsecase getCreateCurrentUserUsecase;
+
+  UserCubit({
+    required this.signInUsecase,
+    required this.signUpUsecase,
+    required this.signOutUsecase,
+    required this.getCreateCurrentUserUsecase,
+  }) : super(UserInitial());
+
+  Future<void> submitSignIn({required UserEntity user}) async {
+    emit(UserLoadingState());
+    try {
+      await signInUsecase.call(user);
+      emit(UserLoadedState());
+    } catch (_) {
+      emit(UserFailureState());
+    }
+  }
+
+  Future<void> submitSignUp({required UserEntity user}) async {
+    emit(UserLoadingState());
+    try {
+      await signUpUsecase.call(user);
+      await getCreateCurrentUserUsecase.call(user);
+      emit(UserLoadedState());
+    } catch (_) {
+      emit(UserFailureState());
+    }
+  }
 }
