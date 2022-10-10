3c96ec4dddc77da82bbdd04bffc7ade405e25b62
Marcelo Moresco <marcelomoresco0@gmail.com>
2022-10-10 14:52:52 -0300

    add sign Up Page

--- lib/src/features/login/presentation/views/register/sign_up_page.dart
+++ lib/src/features/login/presentation/views/register/sign_up_page.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SignUpPage extends StatelessWidget {
+import '../../../domain/entities/user_entity.dart';
+import '../../blocs/user_cubit/user_cubit.dart';
+
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
 
   @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+
+  void submitSignUp() {
+    if (_usernameController.text.isNotEmpty &&
+        _emailController.text.isNotEmpty &&
+        _passwordController.text.isNotEmpty) {
+      context.read<UserCubit>().submitSignUp(
+            user: UserEntity(
+              email: _emailController.text,
+              password: _passwordController.text,
+            ),
+          );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            "Erro ao tentar login",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      key: _globalKey,
+    );
   }
 }
