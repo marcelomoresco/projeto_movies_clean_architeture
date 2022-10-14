81cd2a6eb6832fb96a6661585bb1911e847eaf38
Marcelo Moresco <marcelomoresco0@gmail.com>
2022-10-14 13:36:21 -0300

    add search page

--- lib/src/features/movies/presentation/views/search/search_page.dart
+++ lib/src/features/movies/presentation/views/search/search_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/search_widget.dart';
+
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            children: const [
+              SearchWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
