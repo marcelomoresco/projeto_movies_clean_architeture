b5c8ca28f525d641ce89751757827de140d88683
Marcelo Moresco <marcelo.moresco@valemobi.com.br>
2022-11-01 14:00:15 -0300

    feat: add forgot and sign_Up

--- lib/src/features/login/presentation/views/forgot/forgot_password_page.dart
+++ lib/src/features/login/presentation/views/forgot/forgot_password_page.dart
@@ -13,6 +13,14 @@ class ForgotPasswordPage extends StatefulWidget {
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   TextEditingController _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final bool _isLoading = false;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
 
   void submitForgotPassword() {
     if (_emailController.text.isNotEmpty) {
@@ -50,6 +58,173 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 530,
+              width: double.infinity,
+              decoration: const BoxDecoration(color: Colors.black),
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 300,
+            child: Container(
+              height: 280,
+              width: MediaQuery.of(context).size.width - 40,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(18),
+                  border: Border.all(color: Colors.white, width: 1.5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.38),
+                      spreadRadius: 5,
+                      blurRadius: 15,
+                    )
+                  ]),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(15.0),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Digite seu email e lhe enviaremos um link!",
+                                  style: TextStyle(
+                                    color: Colors.grey[600],
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            TextFormField(
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Campo Obrigatório";
+                                } else if (!value.contains('@')) {
+                                  return "E-mail Incorreto";
+                                }
+                                return null;
+                              },
+                              controller: _emailController,
+                              obscureText: false,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                  //errorText: _errorEmailText,
+                                  prefixIcon: const Icon(
+                                    Icons.email,
+                                    color: Colors.black,
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.grey.shade500),
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.grey.shade500),
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  contentPadding: const EdgeInsets.all(10),
+                                  hintText: "E-mail",
+                                  hintStyle: const TextStyle(fontSize: 14)),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                          ]),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(22),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.7),
+                                spreadRadius: 5,
+                                blurRadius: 15,
+                              )
+                            ]),
+                        child: _isLoading
+                            ? const Center(
+                                child: CircularProgressIndicator(
+                                    color: Colors.white),
+                              )
+                            : ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.black),
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    submitForgotPassword();
+                                    const snackBar = SnackBar(
+                                      backgroundColor: Colors.green,
+                                      content: Text(
+                                        "E-mail Enviado com Sucesso",
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    );
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(snackBar);
+                                  }
+                                },
+                                child: GestureDetector(
+                                  child: const Text("Mudar Senha",
+                                      style: TextStyle(color: Colors.white)),
+                                ),
+                              ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
