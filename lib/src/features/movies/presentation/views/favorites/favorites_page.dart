5b27815cf726b8c209c1ec7d8173ea78df4b5af3
Marcelo Moresco <marcelomoresco0@gmail.com>
2022-11-06 23:34:25 -0300

    feat: add tap on favorites

--- lib/src/features/movies/presentation/views/favorites/favorites_page.dart
+++ lib/src/features/movies/presentation/views/favorites/favorites_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:projeto_movies_clean_arciteture/src/features/movies/domain/entities/movies_entity.dart';
 import 'package:projeto_movies_clean_arciteture/src/features/movies/presentation/blocs/favorites_bloc/favorites_bloc.dart';
+import 'package:projeto_movies_clean_arciteture/src/features/movies/presentation/blocs/movie_detail/movie_detail_bloc.dart';
+import 'package:projeto_movies_clean_arciteture/src/features/movies/presentation/views/details/movie_detail_page.dart';
 import 'package:projeto_movies_clean_arciteture/src/features/movies/presentation/widgets/loading_widget.dart';
 
 import '../../../domain/entities/movies_details_entity.dart';
@@ -45,114 +47,130 @@ class FavoritesPage extends StatelessWidget {
                       itemBuilder: (context, index) {
                         MoviesEntity movie = state.favorites.movies[index];
                         DateTime dateTime = DateTime.parse(movie.releaseDate);
-                        return Slidable(
-                          key: ValueKey(index),
-                          endActionPane: ActionPane(
-                              motion: const ScrollMotion(),
-                              children: [
-                                SlidableAction(
-                                  backgroundColor: Colors.red,
-                                  icon: Icons.delete,
-                                  label: "Deletar",
-                                  onPressed: (_) {
-                                    context.read<FavoritesBloc>().add(
-                                          RemoveFavoritesEvent(
-                                            movie: movie,
-                                          ),
-                                        );
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                        backgroundColor: Colors.red,
-                                        content: Text(
-                                          "Removido dos favoritos",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ]),
-                          child: Padding(
-                            padding: const EdgeInsets.all(18.0),
-                            child: Container(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
+                        return GestureDetector(
+                          onTap: () {
+                            context
+                                .read<MovieDetailBloc>()
+                                .add(GetMovieDetailsEvent(movie.id));
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    MovieDetailPage(movie: movie),
+                              ),
+                            );
+                          },
+                          child: Slidable(
+                            key: ValueKey(index),
+                            endActionPane: ActionPane(
+                                motion: const ScrollMotion(),
                                 children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(5.0),
-                                    child: CachedNetworkImage(
-                                      imageUrl:
-                                          'https://image.tmdb.org/t/p/original/${movie.posterPath}',
-                                      height:
-                                          MediaQuery.of(context).size.height /
-                                              4,
-                                      fit: BoxFit.cover,
-                                      placeholder: (context, url) =>
-                                          const Center(
-                                              child:
-                                                  CircularProgressIndicator()),
-                                    ),
-                                  ),
-                                  SizedBox(width: 60),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(movie.title,
-                                          style: const TextStyle(
+                                  SlidableAction(
+                                    backgroundColor: Colors.red,
+                                    icon: Icons.delete,
+                                    label: "Deletar",
+                                    onPressed: (_) {
+                                      context.read<FavoritesBloc>().add(
+                                            RemoveFavoritesEvent(
+                                              movie: movie,
+                                            ),
+                                          );
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        const SnackBar(
+                                          backgroundColor: Colors.red,
+                                          content: Text(
+                                            "Removido dos favoritos",
+                                            style: TextStyle(
                                               color: Colors.white,
-                                              fontSize: 22)),
-                                      Text(
-                                          DateFormat("dd/MM/yyyy")
-                                              .format(dateTime),
-                                          style: const TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 12)),
-                                      Row(
-                                        children: [
-                                          const Icon(
-                                            Icons.star,
-                                            color: Colors.yellow,
-                                            size: 14,
-                                          ),
-                                          const Icon(
-                                            Icons.star,
-                                            color: Colors.yellow,
-                                            size: 14,
-                                          ),
-                                          const Icon(
-                                            Icons.star,
-                                            color: Colors.yellow,
-                                            size: 14,
-                                          ),
-                                          const Icon(
-                                            Icons.star,
-                                            color: Colors.yellow,
-                                            size: 14,
-                                          ),
-                                          const Icon(
-                                            Icons.star,
-                                            color: Colors.yellow,
-                                            size: 14,
-                                          ),
-                                          SizedBox(
-                                            width: 5,
-                                          ),
-                                          Text(
-                                            movie.voteAverage,
-                                            style: const TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 16,
                                             ),
                                           ),
-                                        ],
-                                      ),
-                                    ],
+                                        ),
+                                      );
+                                    },
                                   ),
-                                ],
+                                ]),
+                            child: Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Container(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      child: CachedNetworkImage(
+                                        imageUrl:
+                                            'https://image.tmdb.org/t/p/original/${movie.posterPath}',
+                                        height:
+                                            MediaQuery.of(context).size.height /
+                                                4,
+                                        fit: BoxFit.cover,
+                                        placeholder: (context, url) =>
+                                            const Center(
+                                                child:
+                                                    CircularProgressIndicator()),
+                                      ),
+                                    ),
+                                    SizedBox(width: 60),
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(movie.title,
+                                            style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 22)),
+                                        Text(
+                                            DateFormat("dd/MM/yyyy")
+                                                .format(dateTime),
+                                            style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 12)),
+                                        Row(
+                                          children: [
+                                            const Icon(
+                                              Icons.star,
+                                              color: Colors.yellow,
+                                              size: 14,
+                                            ),
+                                            const Icon(
+                                              Icons.star,
+                                              color: Colors.yellow,
+                                              size: 14,
+                                            ),
+                                            const Icon(
+                                              Icons.star,
+                                              color: Colors.yellow,
+                                              size: 14,
+                                            ),
+                                            const Icon(
+                                              Icons.star,
+                                              color: Colors.yellow,
+                                              size: 14,
+                                            ),
+                                            const Icon(
+                                              Icons.star,
+                                              color: Colors.yellow,
+                                              size: 14,
+                                            ),
+                                            SizedBox(
+                                              width: 5,
+                                            ),
+                                            Text(
+                                              movie.voteAverage,
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
