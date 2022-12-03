46dbc458d08b294762f2cfffda651102a8e10eec
Marcelo Moresco <marcelomoresco0@gmail.com>
2022-12-02 22:24:52 -0300

    fix: add modal delete rating

--- lib/src/features/movies/presentation/cubits/review/review_cubit.dart
+++ lib/src/features/movies/presentation/cubits/review/review_cubit.dart
@@ -40,9 +40,7 @@ class ReviewCubit extends Cubit<ReviewState> {
 
   Future<void> getRating(MoviesDetailsEntity moviesDetailsEntity) async {
     try {
-      print("TENTANDO");
       final rating = await getRatingUsecase();
-      print(rating);
       String message = '';
       for (int i = 0; i < rating.length; i++) {
         if (rating[i].id == moviesDetailsEntity.id) {
---
+++ lib/src/features/movies/presentation/widgets/modal_delete_rating.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:projeto_movies_clean_arciteture/src/features/movies/domain/entities/movies_details_entity.dart';
+import 'package:projeto_movies_clean_arciteture/src/features/movies/presentation/cubits/review/review_cubit.dart';
+
+class ModalDeleteRating {
+  final MoviesDetailsEntity moviesDetailsEntity;
+  ModalDeleteRating({required this.moviesDetailsEntity});
+
+  Future<void> modalBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+      ),
+      builder: (BuildContext context) {
+        return Container(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Tem certeza que quer apagar o rating desse filme?",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(Icons.close))
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Ao apagar o seu rating ele deixará de existir em nosso banco\n de dados e não conseguirá puxado de volta",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(Icons.close))
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 16,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      "Não, quero voltar",
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 16,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                    onPressed: () {
+                      context
+                          .read<ReviewCubit>()
+                          .deleteRatingMovie(moviesDetailsEntity.id, context)
+                          .then(
+                            (value) => context
+                                .read<ReviewCubit>()
+                                .getRating(moviesDetailsEntity),
+                          )
+                          .then(
+                            (value) => Navigator.of(context).pop(),
+                          );
+                    },
+                    child: const Text(
+                      "Apagar Rating",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
